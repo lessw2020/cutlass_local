3cb128c21e88f917b73477dbe289231e3632e5f7
lessw2020 <lessw@etrillium.com>
2025-11-23 00:53:09 +0000

    v1 wintergemm working

--- examples/python/CuTeDSL/notebooks/winter_gemm.py
+++ examples/python/CuTeDSL/notebooks/winter_gemm.py
@@ -14,13 +14,250 @@ from cutlass.cute.runtime import from_dlpack
 io_dtype = cutlass.Float16
 acc_dtype = cutlass.Float32
 
-mma_tcgen05_inst_shape_mnk = (128, 256, 16)  # shape of single tcgen05.mma instruction
+mma_inst_shape_mnk = (128, 256, 16)  # shape of single tcgen05.mma instruction
 mma_cta_tiler_mnk = (128, 256, 64)  # block shape that 1 or 2 ctas will process
 threads_per_cta = 128
 
 # Pipeline stage configuration
 ab_stages = 4   # number of stages for A and B matrices
 acc_stage = 1   # number of stages for accumulation before writing to global memory
+
+@cute.struct
+class SharedStorage:
+    ab_mbar_ptr: cute.struct.MemRange[cutlass.Int64, ab_stages * 2]  #contiguous memory for barrier
+    acc_mbar_ptr: cute.struct.MemRange[cutlass.Int64, acc_stage * 2]  #contiguous memory for barrier
+    tmem_holding_buf: cutlass.Int32
+
+
+def prep_tmem(smem_storage: SharedStorage, 
+            tmem_barrier_id: int=1, 
+            threads_per_cta: int=128, 
+            num_tmem_cols: int=512):
+            
+        # Allocate all TMEM columns
+        tmem_alloc_barrier = pipeline.NamedBarrier(
+            barrier_id=tmem_barrier_id,
+            num_threads=threads_per_cta,
+        )
+        tmem = utils.TmemAllocator(
+            smem_storage.tmem_holding_buf,
+            barrier_for_retrieve=tmem_alloc_barrier,
+        )
+        
+        tmem.allocate(num_tmem_cols)
+        return tmem
+
+@cute.kernel
+def kernel(
+    tiled_mma: cute.TiledMma,
+    tma_atom_a: cute.CopyAtom,
+    mA_mkl: cute.Tensor,
+    tma_atom_b: cute.CopyAtom,
+    mB_nkl: cute.Tensor,
+    mC_mnl: cute.Tensor,
+    a_smem_layout: cute.ComposedLayout,
+    b_smem_layout: cute.ComposedLayout,
+):
+    #
+    # 1. Prepare args
+    #
+    # Current thread/warp/block coordinates
+    tidx, _, _ = cute.arch.thread_idx()
+    warp_idx = cute.arch.warp_idx()
+    warp_idx = cute.arch.make_warp_uniform(warp_idx)
+    bidx, bidy, _ = cute.arch.block_idx()
+    mma_coord_mnk = (bidx, bidy, None)
+
+    # Allocate SMEM
+    smem = cutlass.utils.SmemAllocator()
+    storage = smem.allocate(SharedStorage)
+    # Allocate SMEM tensors for A and B
+    sA = smem.allocate_tensor(
+        element_type=io_dtype,
+        layout=a_smem_layout.outer,
+        byte_alignment=128,
+        swizzle=a_smem_layout.inner,
+    )
+    sB = smem.allocate_tensor(
+        element_type=io_dtype,
+        layout=b_smem_layout.outer,
+        byte_alignment=128,
+        swizzle=b_smem_layout.inner,
+    )
+
+    # Allocate all TMEM columns
+    tmem = prep_tmem(storage)
+
+    # Prefetch tma descriptor
+    if warp_idx == 0:
+        cpasync.prefetch_descriptor(tma_atom_a)
+        cpasync.prefetch_descriptor(tma_atom_b)
+
+    
+    # Pipeline configuration
+    num_tma_copy_bytes = cute.size_in_bytes(
+        io_dtype, cute.select(a_smem_layout, mode=[0, 1, 2])
+    ) + cute.size_in_bytes(io_dtype, cute.select(b_smem_layout, mode=[0, 1, 2]))
+
+# tma -> mma pipeline
+    ab_producer, ab_consumer = pipeline.PipelineTmaUmma.create(
+        num_stages=ab_stages,
+        producer_group=pipeline.CooperativeGroup(pipeline.Agent.Thread),
+        consumer_group=pipeline.CooperativeGroup(pipeline.Agent.Thread),
+        tx_count=num_tma_copy_bytes,
+        barrier_storage=storage.ab_mbar_ptr.data_ptr(),
+    ).make_participants()
+
+    # mma acc output -> tma global memory pipeline
+    acc_producer, acc_consumer = pipeline.PipelineUmmaAsync.create(
+        num_stages=acc_stage,
+        producer_group=pipeline.CooperativeGroup(pipeline.Agent.Thread),
+        consumer_group=pipeline.CooperativeGroup(
+            pipeline.Agent.Thread, threads_per_cta
+        ),
+        barrier_storage=storage.acc_mbar_ptr.data_ptr(),
+    ).make_participants()
+
+    # Partition tensors for MMA and make fragments
+    # (bM, bK, RestK)
+    gA = cute.local_tile(mA_mkl, mma_cta_tiler_mnk, mma_coord_mnk, proj=(1, None, 1)) # (128, 256, 64) -> (128, 64) * ab_stages
+    # (bN, bK, RestK)
+    gB = cute.local_tile(mB_nkl, mma_cta_tiler_mnk, mma_coord_mnk, proj=(None, 1, 1)) # (128, 256, 64) -> (256, 64) * ab_stages
+    # (bM, bN)
+    gC = cute.local_tile(mC_mnl, mma_cta_tiler_mnk, mma_coord_mnk, proj=(1, 1, None)) # (128, 256, 64) -> (128, 256) * acc_stage
+    
+    thr_mma = tiled_mma.get_slice(0)
+    # (MMA, MMA_M, MMA_K)
+    # 
+    tCgA = thr_mma.partition_A(gA)
+    # (MMA, MMA_N, MMA_K)
+    tCgB = thr_mma.partition_B(gB)
+    # (MMA, MMA_M, MMA_N)
+    tCgC = thr_mma.partition_C(gC)
+    # (MMA, MMA_M, MMA_K)
+    tCrA = tiled_mma.make_fragment_A(sA)
+    # (MMA, MMA_N, MMA_K)
+    tCrB = tiled_mma.make_fragment_B(sB)
+    # (MMA, MMA_M, MMA_N)
+    acc_shape = tiled_mma.partition_shape_C(mma_cta_tiler_mnk[:2])   # (128, 256, 64) -> (128, 256)
+    # (MMA, MMA_M, MMA_N)
+    tCtAcc = tiled_mma.make_fragment_C(acc_shape)
+
+    # Partition tensors for TMA; This requires the tensors partitioned for MMA
+    tAsA, tAgA = cute.nvgpu.cpasync.tma_partition(
+        tma_atom_a,
+        0,
+        cute.make_layout(1),
+        cute.group_modes(sA, 0, 3),
+        cute.group_modes(tCgA, 0, 3),
+    )
+    tBsB, tBgB = cute.nvgpu.cpasync.tma_partition(
+        tma_atom_b,
+        0,
+        cute.make_layout(1),
+        cute.group_modes(sB, 0, 3),
+        cute.group_modes(tCgB, 0, 3),
+    )
+
+    # CTA-wide sync before retrieving the pointer to the start of the allocated TMEM
+    # Only warp 0 does the allocation so we need to sync before retrieving the TMEM start address
+    tmem.wait_for_alloc()
+    tmem_ptr = tmem.retrieve_ptr(acc_dtype)
+    # Swap the pointer in tCtAcc
+    tCtAcc = cute.make_tensor(tmem_ptr, tCtAcc.layout)
+
+    subtile_cnt = 4
+    # (EpiTile)
+    epi_tiler = (
+        (cute.size(tCtAcc, mode=[0, 0]), cute.size(tCtAcc, mode=[0, 1]) // subtile_cnt),
+    )
+    # (EpiTile, NumTiles)
+    tCtAcc_epi = cute.zipped_divide(tCtAcc, epi_tiler)
+    # (EpiTile, NumTiles)
+    gC_epi = cute.zipped_divide(tCgC, epi_tiler)
+
+    # Every thread loads 32x128 bits
+    tmem_atom = cute.make_copy_atom(
+        tcgen05.Ld32x32bOp(tcgen05.Repetition.x64),
+        cutlass.Float32,
+    )
+    tmem_tiled_copy = tcgen05.make_tmem_copy(tmem_atom, tCtAcc_epi[None, 0])
+    tmem_thr_copy = tmem_tiled_copy.get_slice(tidx)
+
+    # (TmemCpy,NumTmemCpy,NumTiles)
+    tDtC = tmem_thr_copy.partition_S(tCtAcc_epi)
+    # (TmemCpy,NumTmemCpy,NumTiles)
+    tDgC = tmem_thr_copy.partition_D(gC_epi)
+
+    # (TmemCpy,NumTmemCpy)
+    tCrAcc = cute.make_rmem_tensor(tDgC[None, None, 0].shape, acc_dtype)
+    # (TmemCpy,NumTmemCpy)
+    tCrC = cute.make_rmem_tensor(tDgC[None, None, 0].shape, io_dtype)
+
+    #
+    # 2. Main loop
+    #
+    num_k_tiles = cute.size(gA, mode=[2])
+    if warp_idx == 0:
+        # Wait for a empty accumulator buffer
+        acc_empty = acc_producer.acquire_and_advance()
+        for k_tile_idx in cutlass.range(num_k_tiles):
+            # Issue TMA loads
+            ab_empty = ab_producer.acquire_and_advance()
+            cute.copy(
+                tma_atom_a,
+                tAgA[(None, ab_empty.count)],
+                tAsA[(None, ab_empty.index)],
+                tma_bar_ptr=ab_empty.barrier,
+            )
+            cute.copy(
+                tma_atom_b,
+                tBgB[(None, ab_empty.count)],
+                tBsB[(None, ab_empty.index)],
+                tma_bar_ptr=ab_empty.barrier,
+            )
+
+            # Execute one K-block worth of MMA instructions
+            ab_full = ab_consumer.wait_and_advance()
+            num_k_blocks = cute.size(tCrA, mode=[2])
+            for k_block_idx in cutlass.range_constexpr(num_k_blocks):
+                k_block_coord = (None, None, k_block_idx, ab_full.index)
+                cute.gemm(
+                    tiled_mma,
+                    tCtAcc,
+                    tCrA[k_block_coord],
+                    tCrB[k_block_coord],
+                    tCtAcc,
+                )
+                tiled_mma.set(tcgen05.Field.ACCUMULATE, True)
+
+            # Signal that the A/B buffers have been consumed and are ready for the next load
+            ab_full.release()
+
+        # Signal that the accumulator is fully computed
+        acc_empty.commit()
+    #
+    # 3. Epilogue
+    #
+
+    # Release TMEM allocation lock
+    tmem.relinquish_alloc_permit()
+
+    # Wait for the accumulator buffer to be full
+    acc_full = acc_consumer.wait_and_advance()
+
+    # TMEM -> RMEM -> GEMM
+    # Sub-tiling for better instruction-level parallelism
+    for i in cutlass.range(cute.size(tDtC, mode=[2])):
+        cute.copy(tmem_tiled_copy, tDtC[None, None, i], tCrAcc)
+        tCrC.store(tCrAcc.load().to(io_dtype))
+        cute.autovec_copy(tCrC, tDgC[None, None, i])
+    acc_full.release()
+
+    # Deallocate TMEM
+    pipeline.sync(barrier_id=1)
+    tmem.free(tmem_ptr)
+
 
 # host function that constructs the tiled MMA and SMEM layouts, and launches the kernel
 @cute.jit
@@ -45,13 +282,13 @@ def host_function(
     # Construct SMEM layouts for A and B
     a_smem_layout = sm100_utils.make_smem_layout_a(
         tiled_mma,
-        mma_tiler_mnk,
+        mma_cta_tiler_mnk,
         a.element_type,
         ab_stages,
     )
     b_smem_layout = sm100_utils.make_smem_layout_b(
         tiled_mma,
-        mma_tiler_mnk,
+        mma_cta_tiler_mnk,
         b.element_type,
         ab_stages,
     )
@@ -64,19 +301,19 @@ def host_function(
         op,
         a,
         a_smem_layout_one_stage,
-        mma_tiler_mnk,
+        mma_cta_tiler_mnk,
         tiled_mma,
     )
     b_tma_atom, b_tma_tensor = cute.nvgpu.make_tiled_tma_atom_B(
         op,
         b,
         b_smem_layout_one_stage,
-        mma_tiler_mnk,
+        mma_cta_tiler_mnk,
         tiled_mma,
     )
 
     # Launch the kernel
-    grid_shape = cute.ceil_div((*c.layout.shape, 1), mma_tiler_mnk[:2])
+    grid_shape = cute.ceil_div((*c.layout.shape, 1), mma_cta_tiler_mnk[:2])
     kernel(
         tiled_mma,
         a_tma_atom,
@@ -91,19 +328,30 @@ def host_function(
         block=(threads_per_cta, 1, 1),
     )
 
-def prep_tmem(smem_storage: cute.SharedStorage, tmem_barrier_id: int=1, threads_per_cta: int=128, num_tmem_cols: int=512):
-        # Allocate all TMEM columns
-        tmem_alloc_barrier = pipeline.NamedBarrier(
-            barrier_id=tmem_barrier_id,
-            num_threads=threads_per_cta,
-        )
-        tmem = utils.TmemAllocator(
-            smem_storage.tmem_holding_buf,
-            barrier_for_retrieve=tmem_alloc_barrier,
-        )
-        
-        tmem.allocate(num_tmem_cols)
-        return tmem
+
+def benchmark(callable, a_tensor, b_tensor, c_tensor):
+    avg_time_us = cute.testing.benchmark(
+        callable,
+        kernel_arguments=cute.testing.JitArguments(a_tensor, b_tensor, c_tensor),
+        warmup_iterations=1,
+        iterations=2,
+    )
+
+    # Calculate metrics
+
+    # Calculate total float ops calculated:
+    # - M * N * K * 2 (FMA)
+    total_float_ops = m * n * k * 2
+
+    # Calculate achieved TFlops
+    achieved_tflops = total_float_ops / (avg_time_us * 1000000)  # TFlops
+
+    # Print results
+    # ------------
+    print(f"Performance Metrics:")
+    print(f"-------------------")
+    print(f"Kernel execution time: {avg_time_us:.4f} us")
+    print(f"Memory throughput: {achieved_tflops:.2f} tflops")
 
 # Make K-major tensors (torch tensors are row-major)
 def make_tensors(mn, k, dtype):
@@ -114,24 +362,42 @@ def make_tensors(mn, k, dtype):
         .to(dtype=dtype, device="cuda")
     )
 
+if __name__ == "__main__":  
+    # define problem size
+    m, n, k = 8192, 8192, 8192
 
-# define problem size
-m, n, k = 8192, 8192, 8192
+    # make tensors
+    a = make_tensors(m, k, cutlass_torch.dtype(io_dtype))
+    b = make_tensors(n, k, cutlass_torch.dtype(io_dtype))
+    c = make_tensors(m, n, cutlass_torch.dtype(io_dtype))
 
+    a_tensor = (
+        from_dlpack(a)
+        .mark_layout_dynamic(leading_dim=1)
+    )
+    b_tensor = (
+        from_dlpack(b)
+        .mark_layout_dynamic(leading_dim=1)
+    )
+    c_tensor = (
+        from_dlpack(c)
+        .mark_layout_dynamic(leading_dim=1)
+    )
 
-a = make_tensors(m, k, cutlass_torch.dtype(io_dtype))
-b = make_tensors(n, k, cutlass_torch.dtype(io_dtype))
-c = make_tensors(m, n, cutlass_torch.dtype(io_dtype))
+    # Compile the kernel for the specific input types
+    naive_kernel = cute.compile(host_function, a_tensor, b_tensor, c_tensor, kernel)
 
-a_tensor = (
-    from_dlpack(a)
-    .mark_layout_dynamic(leading_dim=1)
-)
-b_tensor = (
-    from_dlpack(b)
-    .mark_layout_dynamic(leading_dim=1)
-)
-c_tensor = (
-    from_dlpack(c)
-    .mark_layout_dynamic(leading_dim=1)
-)
+    # Run the kernel
+    benchmark(naive_kernel, a_tensor, b_tensor, c_tensor)
+
+    # Verify Results
+    # -------------
+    # Compare our kernel output with PyTorch's native implementation
+    # Compute reference result and verify
+    ref = (torch.einsum("mk,nk->mn", a.to(torch.float32), b.to(torch.float32))).cpu()
+    torch.testing.assert_close(
+        c.cpu(), ref.to(cutlass_torch.dtype(io_dtype)), atol=1e-1, rtol=1e-05
+    )
+    print("Verification passed!")
+
+    #host_function(a_tensor, b_tensor, c_tensor,)
